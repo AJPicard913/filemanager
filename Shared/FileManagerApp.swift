8ef03e7c593caf8d5f34a1f92a312f2f0d5f9c85
AJ Picard <Contact@saucedesigns.io>
2020-12-11 10:46:11 -0500

    Initial Commit

---
+++ Shared/FileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
